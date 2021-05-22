de7748ce76dd3c3b006ce458660e010b5d1fea16
mindscan-de <maxim.gansert@gmail.com>
2021-05-22 21:52:41 +0200

    add tests for the constructors.

---
+++ BrightFlux-Core/test/de/mindscan/brightflux/dataframes/DataFrameImplTest.java
@@ -0,0 +1,73 @@
+package de.mindscan.brightflux.dataframes;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+
+public class DataFrameImplTest {
+
+    @Test
+    public void testGetName_CtorWithNameOnlySetFirstName_expectFirstName() throws Exception {
+        // arrange
+        DataFrameImpl dataFrameImpl = new DataFrameImpl( "FirstName" );
+
+        // act
+        String result = dataFrameImpl.getName();
+
+        // assert
+        assertThat( result, equalTo( "FirstName" ) );
+    }
+
+    @Test
+    public void testGetName_CtorWithNameOnlySetSecondName_expectSecondName() throws Exception {
+        // arrange
+        DataFrameImpl dataFrameImpl = new DataFrameImpl( "SecondName" );
+
+        // act
+        String result = dataFrameImpl.getName();
+
+        // assert
+        assertThat( result, equalTo( "SecondName" ) );
+    }
+
+    @Test
+    public void testGetName_CtorWithNameAndUUIDSetFirstName_expectFirstName() throws Exception {
+        // arrange
+        DataFrameImpl dataFrameImpl = new DataFrameImpl( "FirstName", UUID.randomUUID() );
+
+        // act
+        String result = dataFrameImpl.getName();
+
+        // assert
+        assertThat( result, equalTo( "FirstName" ) );
+    }
+
+    @Test
+    public void testGetName_CtorWithNameAndUUIDSetSecondName_expectSecondName() throws Exception {
+        // arrange
+        DataFrameImpl dataFrameImpl = new DataFrameImpl( "SecondName", UUID.randomUUID() );
+
+        // act
+        String result = dataFrameImpl.getName();
+
+        // assert
+        assertThat( result, equalTo( "SecondName" ) );
+    }
+
+    @Test
+    public void testGetName_CtorWithNameAndUUIDSetRandomUUID_expectSameUUID() throws Exception {
+        // arrange
+        UUID expectedUUID = UUID.randomUUID();
+        DataFrameImpl dataFrameImpl = new DataFrameImpl( "SecondName", expectedUUID );
+
+        // act
+        UUID result = dataFrameImpl.getUuid();
+
+        // assert
+        assertThat( result, equalTo( expectedUUID ) );
+    }
+
+}
