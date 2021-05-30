a6515fdcadf37f732eb3d80787b992cb7fb4d55d
mindscan-de <maxim.gansert@gmail.com>
2021-05-30 16:44:19 +0200

    simple implementation for a DataFrameDataBuilder and DataFrameFactory

---
+++ BrightFlux-Core/src/de/mindscan/brightflux/dataframes/DataFrameFactory.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ * MIT License
+ *
+ * Copyright (c) 2021 Maxim Gansert, Mindscan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+package de.mindscan.brightflux.dataframes;
+
+/**
+ * 
+ */
+public class DataFrameFactory {
+
+    public static DataFrameImpl create( String name, String[] columnnames, String[] formats ) {
+        if (columnnames == null || columnnames.length == 0) {
+            return new DataFrameImpl( "unknown" );
+        }
+
+        if (formats == null || formats.length == 0) {
+            throw new IllegalArgumentException( "formats must not be null or empty" );
+        }
+
+        if (columnnames.length != formats.length) {
+            throw new IllegalArgumentException(
+                            "columnnames(length: " + columnnames.length + ") and formats(length: " + formats.length + ") have different length" );
+        }
+
+        DataFrameBuilder builder = new DataFrameBuilder();
+
+        for (int i = 0; i < columnnames.length; i++) {
+            builder.addColumn( columnnames[i], formats[i] );
+        }
+
+        return builder.build();
+    }
+}
