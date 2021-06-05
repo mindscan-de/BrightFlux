8969c10a9d34a3e2efd937b16c963da70cafdac3
mindscan-de <maxim.gansert@gmail.com>
2021-06-05 15:07:24 +0200

    implement first unit test for the csv tokenizer

---
+++ BrightFlux-Core/test/de/mindscan/brightflux/ingest/tokenizers/CSVTokenizerTest.java
@@ -0,0 +1,26 @@
+package de.mindscan.brightflux.ingest.tokenizers;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+
+import java.util.Collection;
+
+import org.junit.jupiter.api.Test;
+
+import de.mindscan.brightflux.ingest.DataToken;
+
+public class CSVTokenizerTest {
+
+    @Test
+    public void testTokenize_emptyString_returnsEmptyTokenList() throws Exception {
+        // arrange
+        CSVTokenizer tokenizer = new CSVTokenizer();
+
+        // act
+        Collection<DataToken> result = tokenizer.tokenize( "" );
+
+        // assert
+        assertThat( result, empty() );
+    }
+
+}
