0d0cee67e3d5882b34dd4161d16d9fa72b74e3a3
mindscan-de <maxim.gansert@gmail.com>
2021-07-26 22:48:05 +0200

    enrich the table UI a bit with a context menu.

--- BrightFlux-Viewer/src/de/mindscan/brightflux/viewer/parts/df/DataFrameTableComposite.java
+++ BrightFlux-Viewer/src/de/mindscan/brightflux/viewer/parts/df/DataFrameTableComposite.java
@@ -26,6 +26,7 @@
 package de.mindscan.brightflux.viewer.parts.df;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.eclipse.jface.layout.TableColumnLayout;
 import org.eclipse.jface.viewers.ColumnPixelData;
@@ -34,18 +35,20 @@ import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MenuDetectEvent;
 import org.eclipse.swt.events.MenuDetectListener;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.wb.swt.SWTResourceManager;
 
 import de.mindscan.brightflux.dataframes.DataFrame;
 import de.mindscan.brightflux.dataframes.DataFrameImpl;
 import de.mindscan.brightflux.dataframes.DataFrameRowFilterPredicate;
 import de.mindscan.brightflux.dataframes.filterpredicate.DataFrameRowFilterPredicateFactory;
+import de.mindscan.brightflux.dataframes.journal.DataFrameJournalEntry;
 import de.mindscan.brightflux.system.commands.BFCommand;
 import de.mindscan.brightflux.system.commands.DataFrameCommandFactory;
 import de.mindscan.brightflux.system.registry.ProjectRegistry;
@@ -80,25 +83,7 @@ public class DataFrameTableComposite extends Composite implements ProjectRegistr
 
         table.addMenuDetectListener( new MenuDetectListener() {
             public void menuDetected( MenuDetectEvent e ) {
-                Point absolute = new Point( e.x, e.y );
-                Point pt = table.toControl( absolute );
-                int columnIndex = columnIndexAtposition( table, pt );
-                if (columnIndex >= 0) {
-                    if (pt.y < table.getHeaderHeight()) {
-                        // header of columnindex (right) clicked....
-
-                        // TODO open a menu here... and then use predicates 
-                        DataFrameRowFilterPredicate predicate = DataFrameRowFilterPredicateFactory.and( // h2.sysctx == 6 && h2.b8==10 
-                                        DataFrameRowFilterPredicateFactory.eq( "h2.sysctx", 6 ), DataFrameRowFilterPredicateFactory.eq( "h2.b8", 10 ) );
-                        BFCommand command = DataFrameCommandFactory.filterDataFrame( ingestedDF, predicate );
-                        DataFrameTableComposite.this.projectRegistry.getCommandDispatcher().dispatchCommand( command );
-                    }
-                    else {
-                        // content of columnindex (right) clicked.
-
-                        // TODO open a menu here...
-                    }
-                }
+                table.getMenu().setVisible( true );
             }
         } );
 
@@ -106,6 +91,84 @@ public class DataFrameTableComposite extends Composite implements ProjectRegistr
         table.setHeaderVisible( true );
         table.setFont( SWTResourceManager.getFont( "Tahoma", 10, SWT.NORMAL ) );
         table.setBounds( 0, 0, 446, 278 );
+
+        Menu menu = new Menu( table );
+        table.setMenu( menu );
+
+        MenuItem mntmDataframe = new MenuItem( menu, SWT.CASCADE );
+        mntmDataframe.setText( "DataFrame" );
+
+        Menu menu_DataFrame = new Menu( mntmDataframe );
+        mntmDataframe.setMenu( menu_DataFrame );
+
+        MenuItem mntmJournal = new MenuItem( menu_DataFrame, SWT.CASCADE );
+        mntmJournal.setText( "Journal" );
+
+        Menu menu_4 = new Menu( mntmJournal );
+        mntmJournal.setMenu( menu_4 );
+
+        MenuItem mntmShowPrint = new MenuItem( menu_4, SWT.NONE );
+        mntmShowPrint.addSelectionListener( new SelectionAdapter() {
+            @Override
+            public void widgetSelected( SelectionEvent e ) {
+                printOrShowDataFrameJournal( ingestedDF );
+            }
+        } );
+        mntmShowPrint.setText( "Show / Print" );
+
+        MenuItem mntmSaveAsReceipt = new MenuItem( menu_4, SWT.NONE );
+        mntmSaveAsReceipt.setText( "Save As Receipt ..." );
+
+        MenuItem mntmSaveToFile = new MenuItem( menu_DataFrame, SWT.NONE );
+        mntmSaveToFile.setText( "Save As ..." );
+
+        MenuItem mntmFilters = new MenuItem( menu, SWT.CASCADE );
+        mntmFilters.setText( "Filters" );
+
+        Menu menu_2 = new Menu( mntmFilters );
+        mntmFilters.setMenu( menu_2 );
+
+        MenuItem mntmevents = new MenuItem( menu_2, SWT.NONE );
+        mntmevents.addSelectionListener( new SelectionAdapter() {
+            @Override
+            public void widgetSelected( SelectionEvent e ) {
+
+//                Point absolute = new Point( e.x, e.y );
+//                Point pt = table.toControl( absolute );
+//                int columnIndex = columnIndexAtposition( table, pt );
+//                if (columnIndex >= 0) {
+//                    if (pt.y < table.getHeaderHeight()) {
+//                        // header of columnindex (right) clicked....
+//                    }
+//                    else {
+//                        // content of columnindex (right) clicked.
+//                        // TODO open a menu here...
+//                    }
+//                }
+
+                apply610filter( ingestedDF );
+            }
+        } );
+        mntmevents.setText( "filter 6.10_events" );
+
+        MenuItem mntmFilterx = new MenuItem( menu_2, SWT.NONE );
+        mntmFilterx.addSelectionListener( new SelectionAdapter() {
+            @Override
+            public void widgetSelected( SelectionEvent e ) {
+                apply666filter( ingestedDF );
+            }
+
+        } );
+        mntmFilterx.setText( "filter 666" );
+
+        MenuItem mntmReceipts = new MenuItem( menu, SWT.CASCADE );
+        mntmReceipts.setText( "Receipts" );
+
+        Menu menu_3 = new Menu( mntmReceipts );
+        mntmReceipts.setMenu( menu_3 );
+
+        MenuItem mntmApplyReceipt = new MenuItem( menu_3, SWT.NONE );
+        mntmApplyReceipt.setText( "Apply Receipt ..." );
     }
 
     /**
@@ -129,18 +192,18 @@ public class DataFrameTableComposite extends Composite implements ProjectRegistr
         this.projectRegistry = projectRegistry;
     }
 
-    private static int columnIndexAtposition( Table table, Point pt ) {
-        int colIndex = -1;
-        TableItem testRow = new TableItem( table, 0 );
-        for (int columnIndex = 0; columnIndex < table.getColumnCount(); columnIndex++) {
-            Rectangle bounds = testRow.getBounds( columnIndex );
-            if ((pt.x >= bounds.x) && (pt.x < (bounds.x + bounds.width))) {
-                colIndex = columnIndex;
-            }
-        }
-        testRow.dispose();
-        return colIndex;
-    }
+//    private static int columnIndexAtposition( Table table, Point pt ) {
+//        int colIndex = -1;
+//        TableItem testRow = new TableItem( table, 0 );
+//        for (int columnIndex = 0; columnIndex < table.getColumnCount(); columnIndex++) {
+//            Rectangle bounds = testRow.getBounds( columnIndex );
+//            if ((pt.x >= bounds.x) && (pt.x < (bounds.x + bounds.width))) {
+//                colIndex = columnIndex;
+//            }
+//        }
+//        testRow.dispose();
+//        return colIndex;
+//    }
 
     private void appenddDataFrameColumns( DataFrame ingestedDF, TableViewer tableViewer, Composite composite ) {
         Collection<String> columnNames = ingestedDF.getColumnNames();
@@ -167,6 +230,28 @@ public class DataFrameTableComposite extends Composite implements ProjectRegistr
     @Override
     protected void checkSubclass() {
         // Disable the check that prevents subclassing of SWT components
+    }
+
+    private void apply610filter( DataFrame dataFrame ) {
+        DataFrameRowFilterPredicate predicate = DataFrameRowFilterPredicateFactory.and( // h2.sysctx == 6 && h2.b8==10 
+                        DataFrameRowFilterPredicateFactory.eq( "h2.sysctx", 6 ), DataFrameRowFilterPredicateFactory.eq( "h2.b8", 10 ) );
+        BFCommand command = DataFrameCommandFactory.filterDataFrame( dataFrame, predicate );
+        DataFrameTableComposite.this.projectRegistry.getCommandDispatcher().dispatchCommand( command );
+    }
+
+    private void apply666filter( DataFrame dataFrame ) {
+        DataFrameRowFilterPredicate predicate = DataFrameRowFilterPredicateFactory.containsStr( "h2.msg", "0x666" );
+        BFCommand command = DataFrameCommandFactory.filterDataFrame( dataFrame, predicate );
+        DataFrameTableComposite.this.projectRegistry.getCommandDispatcher().dispatchCommand( command );
+
+    }
+
+    private void printOrShowDataFrameJournal( DataFrame dataFrame ) {
+        System.out.println( "DataFrameLog:" );
+        List<DataFrameJournalEntry> journalEntries = dataFrame.getJournal().getJournalEntries();
+        for (DataFrameJournalEntry entry : journalEntries) {
+            System.out.println( ": " + entry.getLogMessage() );
+        }
     }
 
 }
