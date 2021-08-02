b2c6a2d6087e815db85d3af4fbaa1e997742c628
mindscan-de <maxim.gansert@gmail.com>
2021-08-02 22:44:54 +0200

    end-to-end testing of the brightflux recipes - execute from bfrecipe.

--- BrightFlux-Viewer/src/de/mindscan/brightflux/system/commands/RecipeExecuteCommand.java
+++ BrightFlux-Viewer/src/de/mindscan/brightflux/system/commands/RecipeExecuteCommand.java
@@ -31,6 +31,8 @@ import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import de.mindscan.brightflux.dataframes.DataFrame;
+import de.mindscan.brightflux.dataframes.DataFrameRowFilterPredicate;
+import de.mindscan.brightflux.dataframes.filterpredicate.DataFrameRowFilterPredicateFactory;
 import de.mindscan.brightflux.dataframes.journal.DataFrameJournalEntry;
 import de.mindscan.brightflux.dataframes.journal.DataFrameJournalEntryType;
 import de.mindscan.brightflux.exceptions.NotYetImplemetedException;
@@ -91,10 +93,13 @@ public class RecipeExecuteCommand implements BFCommand {
                             // "parse..." - i mean we only can/must do these at the moment
                             // everything else is just a waste of time currently.
                             if (predicate.equals( "SELECT * FROM df WHERE ( df.'h2.msg'.contains ('0x666') )" )) {
-                                currentDataFrame = currentDataFrame.select().where( predicate );
+                                DataFrameRowFilterPredicate mypredicate = DataFrameRowFilterPredicateFactory.containsStr( "h2.msg", "0x666" );
+                                currentDataFrame = currentDataFrame.select().where( mypredicate );
                             }
                             else if (predicate.equals( "SELECT * FROM df WHERE ( ( df.'h2.sysctx' == 6 )  AND ( df.'h2.b8' == 10 )   )" )) {
-                                currentDataFrame = currentDataFrame.select().where( predicate );
+                                DataFrameRowFilterPredicate mypredicate = DataFrameRowFilterPredicateFactory.and( // h2.sysctx == 6 && h2.b8==10 
+                                                DataFrameRowFilterPredicateFactory.eq( "h2.sysctx", 6 ), DataFrameRowFilterPredicateFactory.eq( "h2.b8", 10 ) );
+                                currentDataFrame = currentDataFrame.select().where( mypredicate );
                             }
                             else {
                                 throw new NotYetImplemetedException();
