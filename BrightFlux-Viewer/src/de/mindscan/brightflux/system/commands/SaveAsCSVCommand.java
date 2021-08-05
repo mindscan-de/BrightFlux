3e203b834810836444480526e8b5128e1c699f93
mindscan-de <maxim.gansert@gmail.com>
2021-08-05 19:39:57 +0200

    create and implement a SaveAs CSV-Command

---
+++ BrightFlux-Viewer/src/de/mindscan/brightflux/system/commands/SaveAsCSVCommand.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ * MIT License
+ *
+ * Copyright (c) 2021 Maxim Gansert, Mindscan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+package de.mindscan.brightflux.system.commands;
+
+import java.nio.file.Path;
+import java.util.function.Consumer;
+
+import de.mindscan.brightflux.dataframes.DataFrame;
+import de.mindscan.brightflux.dataframes.writer.DataFrameWriterFactory;
+import de.mindscan.brightflux.system.events.BFEvent;
+
+/**
+ * 
+ */
+public class SaveAsCSVCommand implements BFCommand {
+
+    private DataFrame inputDataFrame;
+    private Path targetFile;
+
+    public SaveAsCSVCommand( DataFrame inputDataFrame, Path targetFile ) {
+        this.inputDataFrame = inputDataFrame;
+        this.targetFile = targetFile;
+    }
+
+    /** 
+     * {@inheritDoc}
+     */
+    @Override
+    public void execute( Consumer<BFEvent> eventConsumer ) {
+        DataFrameWriterFactory.create( "csv" ).writeToFile( inputDataFrame, targetFile );
+    }
+
+}
