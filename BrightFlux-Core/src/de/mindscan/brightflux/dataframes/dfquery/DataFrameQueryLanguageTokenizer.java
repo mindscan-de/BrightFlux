71c33ebf8155e57d59fecb021c0b93340d0d57af
mindscan-de <maxim.gansert@gmail.com>
2021-08-14 22:10:20 +0200

    add simple and generic tokenizer code skeleton for DFQL.

--- BrightFlux-Core/src/de/mindscan/brightflux/dataframes/dfquery/DataFrameQueryLanguageTokenizer.java
+++ BrightFlux-Core/src/de/mindscan/brightflux/dataframes/dfquery/DataFrameQueryLanguageTokenizer.java
@@ -25,9 +25,130 @@
  */
 package de.mindscan.brightflux.dataframes.dfquery;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import de.mindscan.brightflux.dataframes.dfquery.tokens.DFQLToken;
+
 /**
  * 
  */
 public class DataFrameQueryLanguageTokenizer {
+
+    // At the starting pooint we will start with very basic tokens
+    // but we will also not only support SELECT ALL FROM
+    // but also things like EXECUTE RECIPE "" ON
+    // and CREATE DATAFRAME FROM df.'columnname' USING TOKENIZER "tokenizername"
+
+    public final static String[] keywords = new String[] { "SELECT", "FROM", "WHERE", "ALL" /*, "AS"*/ };
+    public final static String[] operators = new String[] { "==", "!=", "<=", ">=", "<", ">", ".", ",", "*", "+", "-", "!" };
+    public final static String[] whitespace = new String[] { " ", "\t", "\r", "\n" };
+    public final static String[] numbers = new String[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+    public final static String[] parenthesis = new String[] { "(", ")" };
+    public final static String[] quotes = new String[] { "'", "\"" };
+
+    private int tokenStart = 0;
+    private int tokenEnd = 0;
+
+    public Iterator<DFQLToken> tokenize( String dfqlQuery ) {
+        List<DFQLToken> tokens = new LinkedList<>();
+
+        tokenStart = 0;
+        tokenEnd = 0;
+        while (tokenStart < dfqlQuery.length() && tokenEnd < dfqlQuery.length()) {
+            tokenEnd = tokenStart + 1;
+
+            char currentChar = dfqlQuery.charAt( tokenStart );
+
+            Class<? extends DFQLToken> currentTokenType = null;
+
+            if (isWhiteSpace( currentChar )) {
+                currentTokenType = consumeWhiteSpaces( dfqlQuery );
+            }
+            else if (isParenthesis( currentChar )) {
+                currentTokenType = consumeParenthesis( dfqlQuery );
+            }
+            else if (isStartOfOperator( currentChar )) {
+                currentTokenType = consumeOperator( dfqlQuery );
+            }
+            else if (isStartOfQuote( currentChar )) {
+                currentTokenType = consumeQuotedText( dfqlQuery );
+            }
+            else if (isDigit( currentChar )) {
+                currentTokenType = consumeNumber( dfqlQuery );
+            }
+            else if (isStartOfIdentifier( currentChar )) {
+                currentTokenType = consumeIdentifier( dfqlQuery );
+            }
+
+//            if(endPosition==dfqlQuery.length()) {
+//                //TODO produce last token.
+//            }
+
+            if (currentTokenType != null) {
+
+            }
+
+            tokenStart = tokenEnd;
+        }
+
+        return tokens.iterator();
+    }
+
+    private Class<? extends DFQLToken> consumeWhiteSpaces( String dfqlQuery ) {
+        // TODO Whitespace-class
+        return null;
+    }
+
+    private Class<? extends DFQLToken> consumeParenthesis( String dfqlQuery ) {
+        // TODO return open or close parenthesis 
+        return null;
+    }
+
+    private Class<? extends DFQLToken> consumeOperator( String dfqlQuery ) {
+        // TODO return it depends on the operator what kind of operation we return or
+        //      just say it is an operator and then let the parser figure out what kind of operator?
+        return null;
+    }
+
+    private Class<? extends DFQLToken> consumeQuotedText( String dfqlQuery ) {
+        // TODO return Quoted Text class
+        return null;
+    }
+
+    private Class<? extends DFQLToken> consumeNumber( String dfqlQuery ) {
+        return null;
+    }
+
+    private Class<? extends DFQLToken> consumeIdentifier( String dfqlQuery ) {
+        // TODO first consume string, and then check if this string is a keyword.
+        // if it is a keyword, we must return a keyword-class instead of an identifier-class
+        return null;
+    }
+
+    private boolean isWhiteSpace( char currentChar ) {
+        return false;
+    }
+
+    private boolean isParenthesis( char currentChar ) {
+        return false;
+    }
+
+    private boolean isStartOfOperator( char currentChar ) {
+        return false;
+    }
+
+    private boolean isStartOfQuote( char currentChar ) {
+        return false;
+    }
+
+    private boolean isDigit( char currentChar ) {
+        return false;
+    }
+
+    private boolean isStartOfIdentifier( char currentChar ) {
+        return false;
+    }
 
 }
