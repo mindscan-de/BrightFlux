832b997d4b6baadab5153b18263792814940cd6e
mindscan-de <maxim.gansert@gmail.com>
2021-08-30 22:36:48 +0200

    prepare the DataFrameQueryLanguage-Engine; at least start implementation

---
+++ BrightFlux-Core/src/de/mindscan/brightflux/dataframes/dfquery/DataFrameQueryLanguageEngine.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ * MIT License
+ *
+ * Copyright (c) 2021 Maxim Gansert, Mindscan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+package de.mindscan.brightflux.dataframes.dfquery;
+
+import java.util.Iterator;
+
+import de.mindscan.brightflux.dataframes.dfquery.tokens.DFQLToken;
+import de.mindscan.brightflux.dataframes.dfquery.tokens.DFQLTokenProvider;
+
+/**
+ * 
+ */
+public class DataFrameQueryLanguageEngine {
+
+    public void executeDFQuery( DataFrame df, String query ) {
+        DataFrameQueryLanguageParser parser = createParser( query );
+
+        // TODO: compile to statement
+        // TODO: prepare environment for the compilet (bind the df to "df")
+        // TODO: excute the statment using the given dataframe
+        
+        // TODO what to return?
+        // some runtime configuration?
+    }
+
+    private DataFrameQueryLanguageParser createParser( String dfqlQuery ) {
+        DataFrameQueryLanguageTokenizer tokenizer = new DataFrameQueryLanguageTokenizer();
+        tokenizer.setIgnoreWhiteSpace( true );
+
+        Iterator<DFQLToken> tokenIterator = tokenizer.tokenize( dfqlQuery );
+
+        DataFrameQueryLanguageParser parser = new DataFrameQueryLanguageParser();
+        parser.setTokenProvider( new DFQLTokenProvider( tokenIterator ) );
+        return parser;
+    }
+
+}
