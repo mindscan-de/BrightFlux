0b21f2758b64059aa38c7de378eb2a51e9b9eeec
mindscan-de <maxim.gansert@gmail.com>
2021-08-31 22:39:33 +0200

    add generated hashcode / equals contract.

--- BrightFlux-Core/src/de/mindscan/brightflux/dataframes/dfquery/tokens/DFQLToken.java
+++ BrightFlux-Core/src/de/mindscan/brightflux/dataframes/dfquery/tokens/DFQLToken.java
@@ -55,4 +55,38 @@ public class DFQLToken {
     public int getPosition() {
         return position;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + position;
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals( Object obj ) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        
+        DFQLToken other = (DFQLToken) obj;
+        if (position != other.position)
+            return false;
+        if (type != other.type)
+            return false;
+        if (value == null) {
+            if (other.value != null)
+                return false;
+        }
+        else if (!value.equals( other.value ))
+            return false;
+        return true;
+    }
+
 }
