3982c0573af2d7af758e5ec74b74fa2dcb1585e8
mindscan-de <maxim.gansert@gmail.com>
2021-09-04 12:05:21 +0200

    use list for query terminal history and use double clicks and menu items

--- BrightFlux-Viewer/src/de/mindscan/brightflux/viewer/parts/mv/BFDataFrameQueryTerminalViewComposite.java
+++ BrightFlux-Viewer/src/de/mindscan/brightflux/viewer/parts/mv/BFDataFrameQueryTerminalViewComposite.java
@@ -25,14 +25,26 @@
  */
 package de.mindscan.brightflux.viewer.parts.mv;
 
+import java.util.ArrayDeque;
+
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ListViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CCombo;
 import org.eclipse.swt.custom.SashForm;
-import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.KeyAdapter;
 import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.MenuDetectEvent;
+import org.eclipse.swt.events.MenuDetectListener;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.wb.swt.SWTResourceManager;
 
 import de.mindscan.brightflux.dataframes.DataFrame;
@@ -52,7 +64,10 @@ public class BFDataFrameQueryTerminalViewComposite extends Composite implements 
 
     private DataFrame currentSelectedDataFrame = null;
     private ProjectRegistry projectRegistry;
-    private StyledText queryHistoryTextArea;
+    private ListViewer listViewer;
+
+    private ArrayDeque<String> queryHistoryStack = new ArrayDeque<>();
+    private CCombo combo;
 
     /**
      * Create the composite.
@@ -63,12 +78,16 @@ public class BFDataFrameQueryTerminalViewComposite extends Composite implements 
         super( parent, style );
         setLayout( new FillLayout( SWT.HORIZONTAL ) );
 
+        buildLayout();
+    }
+
+    private void buildLayout() {
         SashForm sashForm = new SashForm( this, SWT.VERTICAL );
 
         Composite upperComposite = new Composite( sashForm, SWT.NONE );
         upperComposite.setLayout( new FillLayout( SWT.HORIZONTAL ) );
 
-        CCombo combo = new CCombo( upperComposite, SWT.BORDER );
+        combo = new CCombo( upperComposite, SWT.BORDER );
         combo.addKeyListener( new KeyAdapter() {
             @Override
             public void keyReleased( KeyEvent e ) {
@@ -76,7 +95,7 @@ public class BFDataFrameQueryTerminalViewComposite extends Composite implements 
                     String theQuery = combo.getText();
 
                     addToHistory( theQuery );
-                    combo.setText( "" );
+                    updateComboText( "" );
 
                     executeQuery( theQuery );
                 }
@@ -87,9 +106,64 @@ public class BFDataFrameQueryTerminalViewComposite extends Composite implements 
         Composite lowerComposite = new Composite( sashForm, SWT.NONE );
         lowerComposite.setLayout( new FillLayout( SWT.HORIZONTAL ) );
 
-        queryHistoryTextArea = new StyledText( lowerComposite, SWT.BORDER );
-        queryHistoryTextArea.setFont( SWTResourceManager.getFont( "Courier New", 11, SWT.NORMAL ) );
+        SashForm sashForm_1 = new SashForm( lowerComposite, SWT.VERTICAL );
+
+        listViewer = new ListViewer( sashForm_1, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL );
+        listViewer.setContentProvider( new ArrayContentProvider() );
+        List list = listViewer.getList();
+        list.setFont( SWTResourceManager.getFont( "Courier New", 10, SWT.NORMAL ) );
+        list.addMouseListener( new MouseAdapter() {
+            @Override
+            public void mouseDoubleClick( MouseEvent e ) {
+                String[] selection = ((List) e.getSource()).getSelection();
+                if (selection != null && selection.length > 0) {
+                    executeQuery( selection[0] );
+                }
+            }
+        } );
+
+        buildQueryListContextMenu( list );
+
+        sashForm_1.setWeights( new int[] { 1 } );
         sashForm.setWeights( new int[] { 60, 237 } );
+    }
+
+    private void buildQueryListContextMenu( List list ) {
+        Menu menu = new Menu( list );
+        list.setMenu( menu );
+
+        MenuItem mntmAsInput = new MenuItem( menu, SWT.NONE );
+        mntmAsInput.addSelectionListener( new SelectionAdapter() {
+            @Override
+            public void widgetSelected( SelectionEvent e ) {
+                List list2 = listViewer.getList();
+                String[] selection = list2.getSelection();
+                if (selection != null && selection.length > 0) {
+                    updateComboText( selection[0] + combo.getText() );
+                }
+            }
+        } );
+        mntmAsInput.setText( "As Input" );
+
+        MenuItem mntmExecute = new MenuItem( menu, SWT.NONE );
+        mntmExecute.addSelectionListener( new SelectionAdapter() {
+            @Override
+            public void widgetSelected( SelectionEvent e ) {
+                List list2 = listViewer.getList();
+                String[] selection = list2.getSelection();
+                if (selection != null && selection.length > 0) {
+                    executeQuery( selection[0] );
+                }
+            }
+        } );
+        mntmExecute.setText( "Execute" );
+
+        list.addMenuDetectListener( new MenuDetectListener() {
+            public void menuDetected( MenuDetectEvent e ) {
+                List list2 = listViewer.getList();
+                e.doit = list2.getSelectionCount() > 0;
+            }
+        } );
 
     }
 
@@ -127,10 +201,16 @@ public class BFDataFrameQueryTerminalViewComposite extends Composite implements 
     }
 
     public void addToHistory( String theQuery ) {
-        // Attention: Instead of using the text area as the main storage, we might be using an LRU Cache and rebuild the text on each Change or duplicate
-        // Also we might want to persist this History for the next start.
-        String newCurrentText = theQuery + System.lineSeparator() + queryHistoryTextArea.getText();
-        queryHistoryTextArea.setText( newCurrentText );
+        queryHistoryStack.addFirst( theQuery );
+
+        updateListViewer();
     }
 
+    private void updateListViewer() {
+        listViewer.setInput( queryHistoryStack.toArray() );
+    }
+
+    private void updateComboText( String newComboText ) {
+        combo.setText( newComboText );
+    }
 }
