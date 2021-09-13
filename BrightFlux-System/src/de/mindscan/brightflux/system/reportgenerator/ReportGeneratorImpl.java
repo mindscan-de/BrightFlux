4bc7fbfa90a72eab9ad191793204acde27f53d24
mindscan-de <maxim.gansert@gmail.com>
2021-09-13 21:58:12 +0200

    simplest possible report generator.

---
+++ BrightFlux-System/src/de/mindscan/brightflux/system/reportgenerator/ReportGeneratorImpl.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ * MIT License
+ *
+ * Copyright (c) 2021 Maxim Gansert, Mindscan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+package de.mindscan.brightflux.system.reportgenerator;
+
+/**
+ * 
+ */
+public class ReportGeneratorImpl {
+
+    private static final String TEMPLATE = "%s\n\n{code}\n%s\n{code}\n\n";
+    private String lastMessage = "";
+    private String lastRows = "";
+
+    private StringBuilder sb = new StringBuilder();
+
+    public void startReport() {
+        lastMessage = "";
+        lastRows = "";
+        sb = new StringBuilder();
+    }
+
+    public void addAnnotationMessage( String newMessage ) {
+        buildPendingLines();
+        lastMessage = newMessage;
+        lastRows = "";
+    }
+
+    public void addDataRow( String renderedDataRow ) {
+        if (lastRows.isEmpty()) {
+            lastRows = renderedDataRow;
+        }
+        else {
+            lastRows += "\n" + renderedDataRow;
+        }
+    }
+
+    public String build() {
+        buildPendingLines();
+        return sb.toString();
+    }
+
+    private void buildPendingLines() {
+        if (lastMessage.isEmpty()) {
+            return;
+        }
+        sb.append( String.format( TEMPLATE, lastMessage, lastRows ) );
+    }
+
+}
