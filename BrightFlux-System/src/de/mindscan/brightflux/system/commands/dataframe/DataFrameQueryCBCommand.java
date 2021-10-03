1bcad26c2fa6f0a3f09f16b035a6f0b9cc76467c
mindscan-de <maxim.gansert@gmail.com>
2021-10-03 13:15:46 +0200

    introduce new QueryCB Command

---
+++ BrightFlux-System/src/de/mindscan/brightflux/system/commands/dataframe/DataFrameQueryCBCommand.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ * MIT License
+ *
+ * Copyright (c) 2021 Maxim Gansert, Mindscan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+package de.mindscan.brightflux.system.commands.dataframe;
+
+import java.util.Map;
+import java.util.function.Consumer;
+
+import de.mindscan.brightflux.dataframes.DataFrame;
+import de.mindscan.brightflux.dataframes.DataFrameRowQueryCallback;
+import de.mindscan.brightflux.framework.command.BFCommand;
+import de.mindscan.brightflux.framework.events.BFEvent;
+import de.mindscan.brightflux.system.events.BFEventFactory;
+
+/**
+ * 
+ */
+public class DataFrameQueryCBCommand implements BFCommand {
+
+    private DataFrame inputDataFrame;
+    private String queryString;
+    private Map<String, DataFrameRowQueryCallback> callbacks;
+
+    /**
+     * @param callbacks 
+     * 
+     */
+    public DataFrameQueryCBCommand( DataFrame inputDataFrame, String queryString, Map<String, DataFrameRowQueryCallback> callbacks ) {
+        this.inputDataFrame = inputDataFrame;
+        this.queryString = queryString;
+        this.callbacks = callbacks;
+    }
+
+    /** 
+     * {@inheritDoc}
+     */
+    @Override
+    public void execute( Consumer<BFEvent> eventConsumer ) {
+        DataFrame newDataFrame = inputDataFrame.queryCB( queryString, callbacks );
+        if (newDataFrame != inputDataFrame) {
+            eventConsumer.accept( BFEventFactory.dataframeCreated( newDataFrame, inputDataFrame ) );
+        }
+    }
+
+}
