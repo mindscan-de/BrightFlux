f4b3ff024aa67c1dd253d172e1adb8b35fc8afb4
mindscan-de <maxim.gansert@gmail.com>
2021-10-31 10:53:16 +0100

    add some unit tests for the boolean column (best tested least used...)

--- BrightFlux-Core/test/de/mindscan/brightflux/dataframes/columns/BooleanColumnTest.java
+++ BrightFlux-Core/test/de/mindscan/brightflux/dataframes/columns/BooleanColumnTest.java
@@ -2,9 +2,15 @@ package de.mindscan.brightflux.dataframes.columns;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.sameInstance;
 
 import org.junit.jupiter.api.Test;
+
+import de.mindscan.brightflux.dataframes.DataFrameColumn;
+import de.mindscan.brightflux.dataframes.columntypes.ColumnTypes;
+import de.mindscan.brightflux.dataframes.columntypes.ColumnValueTypes;
 
 public class BooleanColumnTest {
 
@@ -55,6 +61,54 @@ public class BooleanColumnTest {
         // assert
         String result = booleanColumn.getColumnName();
         assertThat( result, equalTo( "Name1" ) );
+    }
+
+    @Test
+    public void testGetColumnType_DefaultCtor_reportsIsColumnTypeBool() throws Exception {
+        // arrange
+        BooleanColumn booleanColumn = new BooleanColumn();
+
+        // act
+        String result = booleanColumn.getColumnType();
+
+        // assert
+        assertThat( result, equalTo( ColumnTypes.COLUMN_TYPE_BOOL ) );
+    }
+
+    @Test
+    public void testGetColumnValueType_DefaultCtor_reportsColumnValueTypeBool() throws Exception {
+        // arrange
+        BooleanColumn booleanColumn = new BooleanColumn();
+
+        // act
+        String result = booleanColumn.getColumnValueType();
+
+        // assert
+        assertThat( result, equalTo( ColumnValueTypes.COLUMN_TYPE_BOOL ) );
+    }
+
+    @Test
+    public void testCloneColumnEmpty_DefaultCtor_IsNotSameInstance() throws Exception {
+        // arrange
+        BooleanColumn booleanColumn = new BooleanColumn();
+
+        // act
+        DataFrameColumn<Boolean> result = booleanColumn.cloneColumnEmpty();
+
+        // assert
+        assertThat( result, not( sameInstance( booleanColumn ) ) );
+    }
+
+    @Test
+    public void testCloneColumnEmpty_DefaultCtorHavingMyName_ColumnHasMyName() throws Exception {
+        // arrange
+        BooleanColumn booleanColumn = new BooleanColumn( "myName" );
+
+        // act
+        String result = booleanColumn.cloneColumnEmpty().getColumnName();
+
+        // assert
+        assertThat( result, equalTo( "myName" ) );
     }
 
 }
