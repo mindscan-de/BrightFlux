e82dcda8b943d34b281de33f97019257b9c34d6a
mindscan-de <maxim.gansert@gmail.com>
2021-11-13 16:24:10 +0100

    refactor: extract methods in IngestBFDataFrameJsonLines, simplify reuse

--- BrightFlux-Core/src/de/mindscan/brightflux/ingest/IngestBFDataFrameJsonLines.java
+++ BrightFlux-Core/src/de/mindscan/brightflux/ingest/IngestBFDataFrameJsonLines.java
@@ -53,6 +53,9 @@ import de.mindscan.brightflux.exceptions.NotYetImplemetedException;
 
 /**
  * TODO: This class needs to be rewritten later using a proper solution for the Gson problem outlined below.
+ * 
+ * Long values are parsed as double, which might have a to small precision compared to long. (e.g. timestamps)
+ * Also we have issues when the data is somehow not properly read/or written? (UTF-8)
  */
 public class IngestBFDataFrameJsonLines {
 
@@ -70,114 +73,125 @@ public class IngestBFDataFrameJsonLines {
             // we know the columns - their types etc.
             // so let's load the first line decode that as JSON
             // into Type JsonLinesDataFrameInfo
-            String firstLine;
-            firstLine = br.readLine();
-            if (firstLine == null) {
-                throw new NotYetImplemetedException( "Actually we should return an empty Dataframe?" );
-            }
+            JsonLinesDataFrameInfo dataFrameInfo = readJsonLinesDataFrameInfo( gson, br );
+            DataFrame dataFrame = readJsonLinesDataFrameContent( gson, br, dataFrameInfo );
 
-            JsonLinesDataFrameInfo dataFrameInfo = gson.fromJson( firstLine.trim(), dataFrameInfoType );
-            List<BFDFColumnInfo> uncompiledColumns = dataFrameInfo.getColumns();
-
-            // --- Compile columns ---
-
-            List<DataFrameColumn<?>> compiledColumns = new LinkedList<>();
-            List<String> columnNames = new LinkedList<>();
-            Map<String, DataFrameColumn<?>> columnMap = new HashMap<>();
-
-            for (BFDFColumnInfo bfdfColumnInfo : uncompiledColumns) {
-                DataFrameColumn<?> compiledColumn = DataFrameColumnFactory.createColumnForType( bfdfColumnInfo.getName(), bfdfColumnInfo.getCType() );
-                compiledColumns.add( compiledColumn );
-                columnNames.add( bfdfColumnInfo.getName() );
-                columnMap.put( bfdfColumnInfo.getName(), compiledColumn );
-            }
-
-            // --- Compile data ---
-
-            // actually we want to be aware of the Types in the column, such that the columnvalues are not double by default, but have the proper type
-            // new GsonBuilder().register...
-
-            String dataRowLine;
-            for (dataRowLine = br.readLine(); dataRowLine != null; dataRowLine = br.readLine()) {
-                // process the line
-                if (dataRowLine.trim().isBlank()) {
-                    break;
-                }
-
-                // The Map Type has a shortcoming ... Because we don't have a proper prototype in java for
-                // a dataframe, GSON uses the javascript object notation specification as the truth - Numbers
-                // are treated as of Double-Type
-
-                // That means, that if we load data, what looks to us as an integer, it is handled as a Double value,
-                // That also has some unexpected limitations, Long values lose precision, that makes things a bit
-                // harder. As for now speaking, gson might be good enough, but probably must be replaced by a
-                // strategy which knows the columnname and its type to parse the JSON data in the correct target 
-                // type. We must either change the implementation which we use for reading JSON files, or we
-                // register some special reader, which can deal with the right targettypes according to the columns.
-                LinkedHashMap<String, Object> dataRow = gson.fromJson( dataRowLine.trim(), dataFrameDataRowType );
-
-                // append this row, depending on the columntype....
-
-                // after that we read each "row" and add data to it, according to the index. 
-                // for the start we will support Sparse_Columns only.
-
-                int idx = gsonHack_DoubeToInt( (Double) dataRow.get( DataFrameSpecialColumns.INDEX_COLUMN_NAME ) );
-                // int org_idx = (Integer) dataRow.get( DataFrameSpecialColumns.ORIGINAL_INDEX_COLUMN_NAME );
-
-                for (String name : columnNames) {
-                    DataFrameColumn<?> column = columnMap.get( name );
-                    if (column instanceof SparseColumn) {
-                        if (dataRow.containsKey( name )) {
-                            Object currentValue = dataRow.get( name );
-
-                            // This is 
-                            if (currentValue instanceof Double) {
-                                Double doubleValue = (Double) currentValue;
-                                String columnValueType = column.getColumnValueType();
-
-                                switch (columnValueType) {
-                                    case ColumnValueTypes.COLUMN_TYPE_INT:
-                                        column.setRaw( idx, gsonHack_DoubeToInt( doubleValue ) );
-                                        break;
-                                    case ColumnValueTypes.COLUMN_TYPE_FLOAT:
-                                        column.setRaw( idx, gsonHack_DoubeToFloat( doubleValue ) );
-                                        break;
-                                    case ColumnValueTypes.COLUMN_TYPE_DOUBLE:
-                                        column.setRaw( idx, doubleValue );
-                                        break;
-                                    default:
-                                        throw new NotYetImplemetedException(
-                                                        "either lose precision or implement another solution for column valuetype='" + columnValueType + "'" );
-                                }
-                            }
-                            else {
-                                column.setRaw( idx, currentValue );
-                            }
-                        }
-                        else {
-                            column.setNA( idx );
-                        }
-                    }
-                    else {
-                        // throw new NotYetImplemetedException("we currently only support SparseColumns"); 
-                    }
-                }
-
-                // We do the hardcore-stuff at first here, and then we will refactor
-                // this into the right places, after we got it working.
-
-                // decide if we appendNA to it...
-            }
-
-            DataFrameBuilder dataFrameBuilder = new DataFrameBuilder( dataFrameInfo.getName() );
-            dataFrameBuilder.addColumns( compiledColumns );
-            return dataFrameBuilder.build();
+            return dataFrame;
         }
         catch (IOException e) {
             e.printStackTrace();
         }
 
         throw new NotYetImplemetedException();
+    }
+
+    public JsonLinesDataFrameInfo readJsonLinesDataFrameInfo( Gson gson, BufferedReader br ) throws IOException {
+        String frameInfoLine = br.readLine();
+
+        if (frameInfoLine == null) {
+            throw new NotYetImplemetedException( "Can't read Data Frame Information Header." );
+        }
+
+        return gson.fromJson( frameInfoLine.trim(), dataFrameInfoType );
+    }
+
+    public DataFrame readJsonLinesDataFrameContent( Gson gson, BufferedReader br, JsonLinesDataFrameInfo dataFrameInfo ) throws IOException {
+        List<BFDFColumnInfo> uncompiledColumns = dataFrameInfo.getColumns();
+
+        // --- Compile columns ---
+
+        List<DataFrameColumn<?>> compiledColumns = new LinkedList<>();
+        List<String> columnNames = new LinkedList<>();
+        Map<String, DataFrameColumn<?>> columnMap = new HashMap<>();
+
+        for (BFDFColumnInfo bfdfColumnInfo : uncompiledColumns) {
+            DataFrameColumn<?> compiledColumn = DataFrameColumnFactory.createColumnForType( bfdfColumnInfo.getName(), bfdfColumnInfo.getCType() );
+            compiledColumns.add( compiledColumn );
+            columnNames.add( bfdfColumnInfo.getName() );
+            columnMap.put( bfdfColumnInfo.getName(), compiledColumn );
+        }
+
+        // --- Compile data ---
+
+        // actually we want to be aware of the Types in the column, such that the columnvalues are not double by default, but have the proper type
+        // new GsonBuilder().register...
+
+        String dataRowLine;
+        for (dataRowLine = br.readLine(); dataRowLine != null; dataRowLine = br.readLine()) {
+            // process the line
+            if (dataRowLine.trim().isBlank()) {
+                break;
+            }
+
+            // The Map Type has a shortcoming ... Because we don't have a proper prototype in java for
+            // a dataframe, GSON uses the javascript object notation specification as the truth - Numbers
+            // are treated as of Double-Type
+
+            // That means, that if we load data, what looks to us as an integer, it is handled as a Double value,
+            // That also has some unexpected limitations, Long values lose precision, that makes things a bit
+            // harder. As for now speaking, gson might be good enough, but probably must be replaced by a
+            // strategy which knows the columnname and its type to parse the JSON data in the correct target 
+            // type. We must either change the implementation which we use for reading JSON files, or we
+            // register some special reader, which can deal with the right targettypes according to the columns.
+            LinkedHashMap<String, Object> dataRow = gson.fromJson( dataRowLine.trim(), dataFrameDataRowType );
+
+            // append this row, depending on the columntype....
+
+            // after that we read each "row" and add data to it, according to the index. 
+            // for the start we will support Sparse_Columns only.
+
+            int idx = gsonHack_DoubeToInt( (Double) dataRow.get( DataFrameSpecialColumns.INDEX_COLUMN_NAME ) );
+            // int org_idx = (Integer) dataRow.get( DataFrameSpecialColumns.ORIGINAL_INDEX_COLUMN_NAME );
+
+            for (String name : columnNames) {
+                DataFrameColumn<?> column = columnMap.get( name );
+                if (column instanceof SparseColumn) {
+                    if (dataRow.containsKey( name )) {
+                        Object currentValue = dataRow.get( name );
+
+                        // This is 
+                        if (currentValue instanceof Double) {
+                            Double doubleValue = (Double) currentValue;
+                            String columnValueType = column.getColumnValueType();
+
+                            switch (columnValueType) {
+                                case ColumnValueTypes.COLUMN_TYPE_INT:
+                                    column.setRaw( idx, gsonHack_DoubeToInt( doubleValue ) );
+                                    break;
+                                case ColumnValueTypes.COLUMN_TYPE_FLOAT:
+                                    column.setRaw( idx, gsonHack_DoubeToFloat( doubleValue ) );
+                                    break;
+                                case ColumnValueTypes.COLUMN_TYPE_DOUBLE:
+                                    column.setRaw( idx, doubleValue );
+                                    break;
+                                default:
+                                    throw new NotYetImplemetedException(
+                                                    "either lose precision or implement another solution for column valuetype='" + columnValueType + "'" );
+                            }
+                        }
+                        else {
+                            column.setRaw( idx, currentValue );
+                        }
+                    }
+                    else {
+                        column.setNA( idx );
+                    }
+                }
+                else {
+                    // throw new NotYetImplemetedException("we currently only support SparseColumns"); 
+                }
+            }
+
+            // We do the hardcore-stuff at first here, and then we will refactor
+            // this into the right places, after we got it working.
+
+            // decide if we appendNA to it...
+        }
+
+        DataFrameBuilder dataFrameBuilder = new DataFrameBuilder( dataFrameInfo.getName() );
+        dataFrameBuilder.addColumns( compiledColumns );
+
+        return dataFrameBuilder.build();
     }
 
     private int gsonHack_DoubeToInt( Double double1 ) {
