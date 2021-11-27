95e47e2acd670fe471ee70755edad8166729acfe
mindscan-de <maxim.gansert@gmail.com>
2021-11-27 21:14:11 +0100

    start simple implementation for querying video information

---
+++ BrightFlux-System/src/de/mindscan/brightflux/system/videoannotator/ffprobe/impl/FFProbeInfoFormat.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ * MIT License
+ *
+ * Copyright (c) 2021 Maxim Gansert, Mindscan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+package de.mindscan.brightflux.system.videoannotator.ffprobe.impl;
+
+import java.util.TreeMap;
+
+/**
+ * 
+ */
+public class FFProbeInfoFormat {
+
+    private String filename;
+    private int nb_streams;
+    private int nb_programs;
+    private String format_name;
+    private String format_long_name;
+    private Double start_time;
+    private Double duration;
+    private Integer size;
+    private String bit_rate;
+    private int probe_score;
+
+    private TreeMap<String, String> tags = new TreeMap<>();
+
+    public Double getDuration() {
+        return duration;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+}
---
+++ BrightFlux-System/src/de/mindscan/brightflux/system/videoannotator/ffprobe/impl/FFProbeInfoStream.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ * MIT License
+ *
+ * Copyright (c) 2021 Maxim Gansert, Mindscan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+package de.mindscan.brightflux.system.videoannotator.ffprobe.impl;
+
+/**
+ * 
+ */
+public class FFProbeInfoStream {
+    private int index;
+    private String codec_name;
+    private String codec_long_name;
+    private String codec_type;
+    private int width = -1;
+    private int height = -1;
+    private String r_frame_rate;
+    private String avg_frame_rate;
+    private int nb_frames;
+
+}
