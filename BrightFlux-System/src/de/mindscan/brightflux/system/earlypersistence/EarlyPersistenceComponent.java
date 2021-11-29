ef5a47cafd913e88745457a0a128cde1bdb17a56
mindscan-de <maxim.gansert@gmail.com>
2021-11-29 21:07:17 +0100

    start simple early persistence component; providing important early data

---
+++ BrightFlux-System/src/de/mindscan/brightflux/system/earlypersistence/EarlyPersistenceComponent.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ * MIT License
+ *
+ * Copyright (c) 2021 Maxim Gansert, Mindscan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+package de.mindscan.brightflux.system.earlypersistence;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+/**
+ * 
+ */
+public class EarlyPersistenceComponent {
+
+    private static final String EARLY_PERSISTENCE_FILENAME = "early.persistence.ini";
+
+    private Path currentDirectory;
+
+    public EarlyPersistenceComponent() {
+        this.currentDirectory = Paths.get( System.getProperty( "user.dir", "." ) );
+    }
+
+    public void initEarlyPersistence() {
+        Path earlyPersistenceFilePath = this.currentDirectory.resolve( EARLY_PERSISTENCE_FILENAME );
+
+        try (FileInputStream earlyPersistenceFileInputStream = new FileInputStream( earlyPersistenceFilePath.toFile() )) {
+            Properties p = new Properties();
+            p.load( earlyPersistenceFileInputStream );
+
+            // TODO: evaluate the content of each Property entry and create a MAP for Path or String values.
+            // lets see what we have to come up with...
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Path getCurrentDirectory() {
+        return currentDirectory;
+    }
+
+}
