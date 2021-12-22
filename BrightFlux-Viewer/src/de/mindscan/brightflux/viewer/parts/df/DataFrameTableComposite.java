cc188d6cb7972b30d24518a50ceab56f7d6770db
mindscan-de <maxim.gansert@gmail.com>
2021-12-22 19:46:24 +0100

    add Search entry to context menu

--- BrightFlux-Viewer/src/de/mindscan/brightflux/viewer/parts/df/DataFrameTableComposite.java
+++ BrightFlux-Viewer/src/de/mindscan/brightflux/viewer/parts/df/DataFrameTableComposite.java
@@ -466,6 +466,35 @@ public class DataFrameTableComposite extends Composite implements ProjectRegistr
         appendFavoriteRecipeShortcutMenus( SystemServices.getInstance(), menu_3 );
         appendFavoriteRecipeShortcutItems( SystemServices.getInstance() );
 
+        MenuItem mntmSearch = new MenuItem( menu, SWT.CASCADE );
+        mntmSearch.setText( "Search" );
+
+        Menu menu_4 = new Menu( mntmSearch );
+        mntmSearch.setMenu( menu_4 );
+
+        MenuItem mntmFuriousironCodeSearch = new MenuItem( menu_4, SWT.NONE );
+        mntmFuriousironCodeSearch.addSelectionListener( new SelectionAdapter() {
+            @Override
+            public void widgetSelected( SelectionEvent e ) {
+                // current selection
+                // create uicommand for search
+                // dispatch the ui command for search
+                // That will hopefully locate the search window
+                // the search window is already subscribed to the kind of events...
+                // and if not open the search window
+                // and then create the event to request the search window to prepare the search based on the current selection
+            }
+        } );
+        mntmFuriousironCodeSearch.setText( "FuriousIron Search" );
+
+        new MenuItem( menu_4, SWT.SEPARATOR );
+
+        MenuItem mntmSearchProfiles = new MenuItem( menu_4, SWT.CASCADE );
+        mntmSearchProfiles.setText( "Search Profiles" );
+
+        Menu menu_5 = new Menu( mntmSearchProfiles );
+        mntmSearchProfiles.setMenu( menu_5 );
+
         new MenuItem( menu, SWT.SEPARATOR );
 
         MenuItem mntmRefreshTableContent = new MenuItem( menu, SWT.NONE );
