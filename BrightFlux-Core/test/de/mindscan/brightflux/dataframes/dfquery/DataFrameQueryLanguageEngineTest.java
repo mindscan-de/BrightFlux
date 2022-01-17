7074ec399f7cbb164242e0bdcf22d2d85462ffa1
mindscan-de <maxim.gansert@gmail.com>
2022-01-17 22:19:37 +0100

    add more unit tests for the ExecuteDFTokenize.

--- BrightFlux-Core/test/de/mindscan/brightflux/dataframes/dfquery/DataFrameQueryLanguageEngineTest.java
+++ BrightFlux-Core/test/de/mindscan/brightflux/dataframes/dfquery/DataFrameQueryLanguageEngineTest.java
@@ -17,11 +17,41 @@ public class DataFrameQueryLanguageEngineTest {
 
         // act
         DataFrame df = null;
-        engine.executeDFTokenize( df, "SELECT df.'__org_idx', df.'h1.ts' TOKENIZE df.'h1.msg' USING 'myjobName' FROM df", recorder::jobRecorder );
+        engine.executeDFTokenize( df, "SELECT df.'__org_idx', df.'h1.ts' TOKENIZE df.'h1.msg' USING 'myTokenizer' FROM df", recorder::jobRecorder );
 
         // assert
         String[] result = recorder.getRecordedjob().getColumnNamesToTransfer();
         assertThat( result, equalTo( new String[] { "__org_idx", "h1.ts" } ) );
+    }
+
+    @Test
+    public void testExecuteDFTokenize_TokenizeStatementExtractColumnNameToTokenize_expectColumnnameToTokenize() throws Exception {
+        // arrange
+        DataFrameQueryLanguageEngine engine = new DataFrameQueryLanguageEngine();
+        JobRecorder recorder = new JobRecorder();
+
+        // act
+        DataFrame df = null;
+        engine.executeDFTokenize( df, "SELECT df.'__org_idx', df.'h1.ts' TOKENIZE df.'h1.msg' USING 'myTokenizer' FROM df", recorder::jobRecorder );
+
+        // assert
+        String result = recorder.getRecordedjob().getInputColumnNameToTokenize();
+        assertThat( result, equalTo( "h1.msg" ) );
+    }
+
+    @Test
+    public void testExecuteDFTokenize_TokenizeStatementExtractProcessorName_expectMyTokenizerAsTokenizer() throws Exception {
+        // arrange
+        DataFrameQueryLanguageEngine engine = new DataFrameQueryLanguageEngine();
+        JobRecorder recorder = new JobRecorder();
+
+        // act
+        DataFrame df = null;
+        engine.executeDFTokenize( df, "SELECT df.'__org_idx', df.'h1.ts' TOKENIZE df.'h1.msg' USING 'myTokenizer' FROM df", recorder::jobRecorder );
+
+        // assert
+        String result = recorder.getRecordedjob().getIngestProcessorName();
+        assertThat( result, equalTo( "myTokenizer" ) );
     }
 
 }
