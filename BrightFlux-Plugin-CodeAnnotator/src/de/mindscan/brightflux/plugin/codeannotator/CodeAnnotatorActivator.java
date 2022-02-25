a1fba7fc0de838ea6cae9ed71f26fc656c0a7340
mindscan-de <maxim.gansert@gmail.com>
2022-02-25 20:45:02 +0100

    start basic initialization fragment for the code annotator persistence

--- BrightFlux-Plugin-CodeAnnotator/src/de/mindscan/brightflux/plugin/codeannotator/CodeAnnotatorActivator.java
+++ BrightFlux-Plugin-CodeAnnotator/src/de/mindscan/brightflux/plugin/codeannotator/CodeAnnotatorActivator.java
@@ -27,6 +27,7 @@ package de.mindscan.brightflux.plugin.codeannotator;
 
 import de.mindscan.brightflux.exceptions.NotYetImplemetedException;
 import de.mindscan.brightflux.framework.registry.ProjectRegistry;
+import de.mindscan.brightflux.system.earlypersistence.BasePersistenceModule;
 import de.mindscan.brightflux.system.services.StartupParticipant;
 import de.mindscan.brightflux.system.services.SystemServices;
 
@@ -35,11 +36,16 @@ import de.mindscan.brightflux.system.services.SystemServices;
  */
 public class CodeAnnotatorActivator implements StartupParticipant {
 
+    private static final String CODE_ANNOTATOR_PLUGIN_PERSISTENCE_NAMESPACE = "code-annotator-plugin";
+
     /** 
      * {@inheritDoc}
      */
     @Override
     public void start( SystemServices systemServices ) {
+        BasePersistenceModule codeAnnotatorBasePersistenceModule = systemServices.getBasePersistenceModule( CODE_ANNOTATOR_PLUGIN_PERSISTENCE_NAMESPACE );
+        // TODO: initialize a PersistenceModule for the code annotator plugin
+
         CodeAnnotatorComponent codeAnnotator = new CodeAnnotatorComponent();
 
         systemServices.registerService( codeAnnotator, CodeAnnotatorComponent.class );
